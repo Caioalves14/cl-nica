9a6e46485f25b21f57536293a4fa9ac844e27ee4
Brenda <132839420+brendamamedio@users.noreply.github.com>
2024-08-22 11:07:17 +0000

    Configurações do angular

--- Clinica-Frontend/src/app/app.component.ts
+++ Clinica-Frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button'; // Importando diretamente no componente
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, ButtonModule],
+  template: '<router-outlet></router-outlet>', 
 })
 export class AppComponent {
   title = 'Clinica-Frontend';
--- Clinica-Frontend/src/app/app.routes.ts
+++ Clinica-Frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RemedyComponent } from './pages/pages/remedy/remedy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RemedyComponent },
+];
